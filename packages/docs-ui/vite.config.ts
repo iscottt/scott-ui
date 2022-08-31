5d0faed006a71b3fb3320af8825c16f23dbcd1df
Scott <1194379729@qq.com>
2022-08-31 21:26:37 +0800

    fix: remove button components name attr

--- packages/docs-ui/vite.config.ts
+++ packages/docs-ui/vite.config.ts
@@ -1,12 +1,4 @@
 import { defineConfig } from 'vite';
-import vueJsx from '@vitejs/plugin-vue-jsx';
-import Unocss from '../scott-ui/plugins/unocss';
 // https://vitejs.dev/config/
 
-export default defineConfig({
-  plugins: [
-    // 添加JSX插件
-    vueJsx(),
-    Unocss(),
-  ],
-});
+export default defineConfig({});
--- packages/scott-ui/src/button/index.ts
+++ packages/scott-ui/src/button/index.ts
@@ -1,12 +1,11 @@
-import Button from "./Button";
-import { App } from "vue";
+import Button from './Button';
+import { App } from 'vue';
 
 // 导出Button组件
 export { Button };
 
 // 导出Vue插件
 export default {
-  name: "SButton",
   install(app: App) {
     app.component(Button.name, Button);
   },
