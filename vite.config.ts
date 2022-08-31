974daed0b71eb05e2aad9bf2d3e880d6ad2911ef
Scott <1194379729@qq.com>
2022-08-31 15:09:51 +0800

    ci: add github action

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import { setupVitePlugins } from './plugins';
+import { defineConfig } from "vite";
+import { setupVitePlugins } from "./plugins";
 // 配置导出模块类型
 const rollupOptions = {
-  external: ['vue', 'vue-router'],
+  external: ["vue", "vue-router"],
   output: {
     globals: {
-      vue: 'Vue',
+      vue: "Vue",
     },
   },
 };
@@ -14,21 +14,22 @@ export default defineConfig({
   plugins: setupVitePlugins(),
   test: {
     globals: true,
-    environment: 'happy-dom',
+    environment: "happy-dom",
     transformMode: {
-      web: [/.[tj]sx$/]
-    }
+      web: [/.[tj]sx$/],
+    },
   },
   build: {
     rollupOptions,
     minify: false,
     cssCodeSplit: true,
+    sourcemap: true, // 开通后可以在chrome中断点调试
     lib: {
-      entry: './src/entry.ts',
-      name: 'ScottUI',
-      fileName: 'scott-ui',
+      entry: "./src/entry.ts",
+      name: "ScottUI",
+      fileName: "scott-ui",
       // 导出模块格式
-      formats: ['es', 'umd', 'iife'],
+      formats: ["es", "umd", "iife"],
     },
   },
 });
